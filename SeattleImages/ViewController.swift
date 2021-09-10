3833786556e72e873c6a77030d6a88596d30a4ce
Xuanwei Zhang <christinezxw@hotmail.com>
2021-09-09 20:56:03 -0700

    add button and image view, add constrains

--- SeattleImages/ViewController.swift
+++ SeattleImages/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func showNextPicture(_ sender: Any) {
+        let num = Int.random(in: 1...10)
+        let imageName = "Seattle\(num)"
+        ImageView.image = UIImage(named: imageName)
+    }
 }
 
